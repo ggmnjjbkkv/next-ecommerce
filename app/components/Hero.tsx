850d0b7292dccdd0e2de7c8944ecbfd76a8a3d43
Gerald Gonzales <94728635+ggmnjjbkkv@users.noreply.github.com>
2024-01-05 11:11:30 +0800

    commit

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import { client, urlFor } from "../lib/sanity";
+import Link from "next/link";
 
 async function getData() {
-  const query = "*[ type == 'heroImages'] [0]";
+  const query = "*[_type == 'heroImages'] [0]";
 
   const data = await client.fetch(query)
 
@@ -23,16 +24,49 @@ export default async function Hero() {
             We are the best so come and shop with us.
           </p>
         </div>
+
         <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
-          <div className="relative left-12 top-12 z-10 -m-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md-top-16 lg:ml-0">
-            <Image 
-            src={urlFor(data.image1).url()}
-            alt="Good Photo"
+          <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md-top-16 lg:ml-0">
+          <Image
+              src={urlFor(data.image1).url()}
+              alt="Great Photo"
+              className="h-full w-full object-cover object-center"
+              priority
+              width={500}
+              height={500}
+            />
+
+          </div>
+          <div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg">
+            <Image src={urlFor(data.image2).url()}
+            alt="Great Photo"
             className="h-full w-full object-cover object-center"
             width={500}
-            height={500} 
+            height={500}
+            priority
             />
+
           </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-between gap-8 md:flex-row">
+        <div className="flex h-12 w-64 divide-x overflow-hidden rounded-lg border">
+          <Link 
+          href="/Men" 
+          className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">
+            Men
+          </Link>
+          <Link 
+          href="/Women" 
+          className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">
+            Women
+          </Link>
+          <Link 
+          href="/Teens" 
+          className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">
+            Teens
+          </Link>
         </div>
       </div>
     </section>
---
+++ app/product/[slug]/page.tsx
@@ -0,0 +1,29 @@
+import { client } from "@/app/lib/sanity";
+
+async function getData(slug: string) {
+  const query = `*[_type == "product" && slug.current == "${slug}"] [0] {
+    _id,
+      images,
+      price,
+      name,
+      description,
+      "slug": slug.current,
+      "categoryName": category->name,
+      
+  }`;
+
+  const data = await client.fetch(query)
+
+  return data;
+}
+
+export default async function ProductPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const data = await getData(params.slug)
+  return (
+    <h1>Hello from Product</h1>
+  )
+}
