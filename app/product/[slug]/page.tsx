330f875a0fad05b8fbbdd612d45d24950ab0c880
Gerald Gonzales <94728635+ggmnjjbkkv@users.noreply.github.com>
2024-01-05 14:09:01 +0800

    me

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import ImageGallery from "@/app/components/ImageGallery";
 import { fullProduct } from "@/app/interface";
 import { client } from "@/app/lib/sanity";
+import { Button } from "@/components/ui/button";
+import { Star, Truck } from "lucide-react";
 
 async function getData(slug: string) {
   const query = `*[_type == "product" && slug.current == "${slug}"] [0] {
@@ -32,6 +34,34 @@ export default async function ProductPage({
       <div className="mx-auto max-w-screen-xl px-4 md:px-8">
         <div className="grid gap-8 md:grid-cols-2">
           <ImageGallery images={data.images} />
+          <div className="md-py-8">
+            <div className="mb-2 md:mb-3">
+              <span className="mb-0.5 inline-block text-gray-500">{data.categoryName}</span>
+              <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">{data.name}</h2>
+            </div>
+            <div className="mb-6 flex items-cemter gap-3 md:mb-10">
+              <Button className="rounded-full gap-x-2">
+                <span className="text-sm">4.5</span>
+                <Star className="h-5 w-5" />
+              </Button>
+
+              <span className="text-sm text-gray-500 transition duration-100 py-2.5">56 Ratings</span>
+            </div>
+            <div className="mb-4">
+              <div className="flex items-end gap-2">
+                <span className="text-xl font-bold text-gray-800 md:text-2xl">
+                  ${data.price}
+                </span>
+                <span className="mb-0.5 text-red-500 line-through">
+                  ${data.price + 30}
+                </span>
+              </div>
+              <span className="text-sm text-gray-500">Incl. Vat plus shipping</span>
+            </div>
+            <div className="mb-6 flex items-center gap-2 text-gray-500">
+              <Truck />
+            </div>
+          </div>
         </div>
       </div>
     </div>
